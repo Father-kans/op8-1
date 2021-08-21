382de77594fa6675171331f677b9d591b17c3699
SangJo Kim <45070398+KimSangJo@users.noreply.github.com>
2021-08-21 22:49:15 +0900

    fix to Comma

--- selfdrive/ui/qt/sidebar.cc
+++ selfdrive/ui/qt/sidebar.cc
@@ -2,12 +2,10 @@
 
 #include <QMouseEvent>
 
-#include "selfdrive/common/util.h"
-#include "selfdrive/hardware/hw.h"
 #include "selfdrive/ui/qt/util.h"
 
 void Sidebar::drawMetric(QPainter &p, const QString &label, const QString &val, QColor c, int y) {
-  const QRect rect = {30, y, 240, val.isEmpty() ? (label.contains("\n") ? 130 : 130) : 135};
+  const QRect rect = {30, y, 240, val.isEmpty() ? (label.contains("\n") ? 124 : 100) : 148};
 
   p.setPen(Qt::NoPen);
   p.setBrush(QBrush(c));
@@ -23,14 +21,14 @@ void Sidebar::drawMetric(QPainter &p, const QString &label, const QString &val, 
 
   p.setPen(QColor(0xff, 0xff, 0xff));
   if (val.isEmpty()) {
-    configFont(p, "Open Sans", 40, "Regular");
-    const QRect r = QRect(rect.x() + 35, rect.y(), rect.width() - 50, rect.height());
+    configFont(p, "Open Sans", 35, "Bold");
+    const QRect r = QRect(rect.x() + 30, rect.y(), rect.width() - 40, rect.height());
     p.drawText(r, Qt::AlignCenter, label);
   } else {
-    configFont(p, "Open Sans", 58, "Regular");
-    p.drawText(rect.x() + 50, rect.y() + 61, val);
+    configFont(p, "Open Sans", 58, "Bold");
+    p.drawText(rect.x() + 50, rect.y() + 71, val);
     configFont(p, "Open Sans", 35, "Regular");
-    p.drawText(rect.x() + 50, rect.y() + 35 + 77, label);
+    p.drawText(rect.x() + 50, rect.y() + 50 + 77, label);
   }
 }
 
@@ -58,35 +56,32 @@ void Sidebar::updateState(const UIState &s) {
   setProperty("netType", network_type[deviceState.getNetworkType()]);
   int strength = (int)deviceState.getNetworkStrength();
   setProperty("netStrength", strength > 0 ? strength + 1 : 0);
-  setProperty("wifiAddr", deviceState.getWifiIpAddress().cStr());
 
-  bool online = net_type != network_type[cereal::DeviceState::NetworkType::NONE];
-  setProperty("connectStr",  online ? "인터넷\n연결됨" : "인터넷\n오프라인");
-  setProperty("connectStatus", online ? good_color : danger_color);
-  m_battery_img = deviceState.getBatteryStatus() == "Charging" ? 1 : 0;
-  m_batteryPercent = deviceState.getBatteryPercent();
+  ItemStatus connectstatus;
+  auto last_ping = deviceState.getLastAthenaPingTime();
+  if (last_ping == 0) {
+    connectstatus = params.getBool("PrimeRedirected") ? ItemStatus{"NO\nPRIME", danger_color} : ItemStatus{"CONNECT\nOFFLINE", warning_color};
+  } else {
+    connectstatus = nanos_since_boot() - last_ping < 80e9 ? ItemStatus{"CONNECT\nONLINE", good_color} : ItemStatus{"CONNECT\nERROR", danger_color};
+  }
+  setProperty("connectStatus", QVariant::fromValue(connectstatus));
 
-  QColor tempStatus = danger_color;
+  QColor tempColor = danger_color;
   auto ts = deviceState.getThermalStatus();
   if (ts == cereal::DeviceState::ThermalStatus::GREEN) {
-    tempStatus = good_color;
+    tempColor = good_color;
   } else if (ts == cereal::DeviceState::ThermalStatus::YELLOW) {
-    tempStatus = warning_color;
+    tempColor = warning_color;
   }
-  setProperty("tempStatus", tempStatus);
-  setProperty("tempVal", (int)deviceState.getAmbientTempC());
+  setProperty("tempStatus", QVariant::fromValue(ItemStatus{QString("%1°C").arg((int)deviceState.getAmbientTempC()), tempColor}));
 
-  QString pandaStr = "차량\n연결됨";
-  QColor pandaStatus = good_color;
+  ItemStatus pandaStatus = {"VEHICLE\nONLINE", good_color};
   if (s.scene.pandaType == cereal::PandaState::PandaType::UNKNOWN) {
-    pandaStatus = danger_color;
-    pandaStr = "차량\n연결안됨";
-  } /*else if (s.scene.started && !sm["liveLocationKalman"].getLiveLocationKalman().getGpsOK()) {
-    pandaStatus = warning_color;
-    pandaStr = "GPS\n찾는중";
-  }*/
-  setProperty("pandaStr", pandaStr);
-  setProperty("pandaStatus", pandaStatus);
+    pandaStatus = {"NO\nPANDA", danger_color};
+  } else if (s.scene.started && !sm["liveLocationKalman"].getLiveLocationKalman().getGpsOK()) {
+    pandaStatus = {"GPS\nSEARCHING", warning_color};
+  }
+  setProperty("pandaStatus", QVariant::fromValue(pandaStatus));
 }
 
 void Sidebar::paintEvent(QPaintEvent *event) {
@@ -94,21 +89,7 @@ void Sidebar::paintEvent(QPaintEvent *event) {
   p.setPen(Qt::NoPen);
   p.setRenderHint(QPainter::Antialiasing);
 
-  //battery
-  QRect  rect(45, 293, 96, 36);
-  QRect  bq(50, 298, int(76* m_batteryPercent * 0.01), 25);
-  QBrush bgBrush("#149948");
-  p.fillRect(bq,  bgBrush);
-  p.drawImage(rect, battery_imgs[m_battery_img]);
-
-  p.setPen(Qt::white);
-  configFont(p, "Open Sans", 30, "Regular");
-
-  char battery_str[32];
-
-  const QRect bt = QRect(170, 288, event->rect().width(), 50);
-  snprintf(battery_str, sizeof(battery_str), "%d%%", m_batteryPercent);
-  p.drawText(bt, Qt::AlignLeft, battery_str);
+  p.fillRect(rect(), QColor(57, 57, 57));
 
   // static imgs
   p.setOpacity(0.65);
@@ -125,19 +106,13 @@ void Sidebar::paintEvent(QPaintEvent *event) {
     x += 37;
   }
 
-  configFont(p, "Open Sans", 30, "Regular");
+  configFont(p, "Open Sans", 35, "Regular");
   p.setPen(QColor(0xff, 0xff, 0xff));
-
-  const QRect r = QRect(-15, 237, event->rect().width(), 50);
-
-  if(Hardware::EON() && net_type == network_type[cereal::DeviceState::NetworkType::WIFI])
-    p.drawText(r, Qt::AlignCenter, wifi_addr);
-  else
-    p.drawText(r, Qt::AlignCenter, net_type);
+  const QRect r = QRect(50, 247, 100, 50);
+  p.drawText(r, Qt::AlignCenter, net_type);
 
   // metrics
-  configFont(p, "Open Sans", 35, "Regular");
-  drawMetric(p, "온도", QString("%1°C").arg(temp_val), temp_status, 355);
-  drawMetric(p, panda_str, "", panda_status, 518);
-  drawMetric(p, connect_str, "", connect_status, 676);
+  drawMetric(p, "TEMP", temp_status.first, temp_status.second, 338);
+  drawMetric(p, panda_status.first, "", panda_status.second, 518);
+  drawMetric(p, connect_status.first, "", connect_status.second, 676);
 }
