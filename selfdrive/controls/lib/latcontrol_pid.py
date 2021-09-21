820cfdeff811ab602582e16315fe164d35e5f1be
SangJo Kim <45070398+KimSangJo@users.noreply.github.com>
2021-09-21 09:53:12 +0900

    Fix PID
    Fix PID
    
    Fix PID

--- selfdrive/controls/lib/latcontrol_pid.py
+++ selfdrive/controls/lib/latcontrol_pid.py
@@ -1,6 +1,6 @@
 import math
 
-from selfdrive.controls.lib.pid import LatPIDController
+from selfdrive.controls.lib.pid import PIDController
 from selfdrive.controls.lib.drive_helpers import get_steer_max
 from cereal import log
 from selfdrive.kegman_kans_conf import kegman_kans_conf
@@ -14,7 +14,7 @@ class LatControlPID():
                             (CP.lateralTuning.pid.kiBP, CP.lateralTuning.pid.kiV),
                             (CP.lateralTuning.pid.kdBP, CP.lateralTuning.pid.kdV),
                             k_f=CP.lateralTuning.pid.kf, pos_limit=1.0, neg_limit=-1.0, 
-                            sat_limit=CP.steerLimitTimer)
+                            sat_limit=CP.steerLimitTimer, derivative_period=0.1)
     self.mpc_frame = 0
 
   def reset(self):
@@ -35,7 +35,7 @@ class LatControlPID():
                             (CP.lateralTuning.pid.kiBP, self.steerKiV),
                             (CP.lateralTuning.pid.kdBP, self.steerKdV),
                             k_f=self.steerKf, pos_limit=1.0, neg_limit=-1.0,
-                            sat_limit=self.steerLimitTimer)
+                            sat_limit=self.steerLimitTimer, derivative_period=0.1)
         self.deadzone = float(self.kegman_kans.conf['deadzone'])
         
       self.mpc_frame = 0    
--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -1,8 +1,8 @@
 from cereal import car
 from common.numpy_fast import clip, interp
-from selfdrive.controls.lib.pid import LongPIController
 from selfdrive.controls.lib.drive_helpers import CONTROL_N
 from selfdrive.modeld.constants import T_IDXS
+from selfdrive.controls.lib.pid import PIDController
 from selfdrive.kegman_kans_conf import kegman_kans_conf
 
 kegman_kans = kegman_kans_conf()
@@ -63,11 +63,12 @@ def long_control_state_trans(active, long_control_state, v_ego, v_target, v_pid,
 class LongControl():
   def __init__(self, CP):
     self.long_control_state = LongCtrlState.off  # initialized to off
-    self.pid = LongPIController((CP.longitudinalTuning.kpBP, CP.longitudinalTuning.kpV),
-                            (CP.longitudinalTuning.kiBP, CP.longitudinalTuning.kiV),
-                            ([0], [0]),
-                            rate=RATE,
-                            sat_limit=0.8)
+    self.pid = PIDController((CP.longitudinalTuning.kpBP, CP.longitudinalTuning.kpV),
+                             (CP.longitudinalTuning.kiBP, CP.longitudinalTuning.kiV),
+                             (CP.longitudinalTuning.kdBP, CP.longitudinalTuning.kdV),
+                             rate=RATE,
+                             sat_limit=0.8,
+                             derivative_period=0.5)
     self.v_pid = 0.0
     self.last_output_accel = 0.0
 
